00d40c9f36393b1506c6b70967484edfd56ecff7
srinzema <s.l.rinzema@gmail.com>
2025-09-22 19:07:21 +0200

    added weighted pearson

--- WeightedSigmoid.py
+++ WeightedSigmoid.py
@@ -1,19 +1,25 @@
 import numpy as np
 
 
-def construct_weights(size, start_diagonal=0, start_weight=0.0, step_size=0.1):
-    weights = []
-    for row in range(size):
-        # diagonal offset = row index
-        for col in range(row, size):
-            diagonal = col - row
-            weight = start_weight + step_size * diagonal
-            weights.append(weight)
-    return np.array(weights)
+def construct_weights(size, start_weight=0.0, step_size=0.1):
+    idx = np.triu_indices(size, k=0)
+    stratum_offsets = idx[1] - idx[0]
+    w_base = start_weight + step_size * np.arange(size)
+    return w_base[stratum_offsets]
 
 
-def weighted_cosine(matrix_a, matrix_b, weights):
-    weighted_dot = np.sum(matrix_a * matrix_b * weights)
-    norm_a = np.sqrt(np.sum(weights * matrix_a**2))
-    norm_b = np.sqrt(np.sum(weights * matrix_b**2))
-    return weighted_dot / (norm_a * norm_b)
+def weighted_cosine(flat_vectors, weights):
+    weighted = flat_vectors * weights
+    norms = np.linalg.norm(weighted, axis=1, keepdims=True)  # shape (n_matrices,1)
+    sim_matrix = weighted @ weighted.T
+    sim_matrix /= norms @ norms.T
+    return sim_matrix
+
+
+def weighted_pearson(flat_vectors, weights):
+    weighted = flat_vectors * weights
+    weighted_centered = weighted - weighted.mean(axis=1, keepdims=True)
+    norms = np.linalg.norm(weighted_centered, axis=1, keepdims=True)
+    sim_matrix = weighted_centered @ weighted_centered.T
+    sim_matrix /= norms @ norms.T
+    return sim_matrix
